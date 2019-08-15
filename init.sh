89c126fd74d3eff52ad0bd7b4b0fe156a75c2e74
GusAntoniassi <gus.antoniassi@gmail.com>
2019-08-15 00:30:31 -0300

    Fixing the init.sh script again because the previous one was selecting the parent dir

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-echo $DIR
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 for f in "$DIR"/*.rc; do
     source "$f";
